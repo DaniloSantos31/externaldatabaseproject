8984d15d0ef872448d13f62582e54f0fd1af72db
DaniloSantos31 <dandan.santos3103@gmail.com>
2024-10-03 09:25:07 -0300

    add UserController

--- src/main/java/com/example/projetoaos/externaldatabase/demo/controller/UserController.java
+++ src/main/java/com/example/projetoaos/externaldatabase/demo/controller/UserController.java
@@ -1,5 +1,69 @@
 package com.example.projetoaos.externaldatabase.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.projetoaos.externaldatabase.demo.model.User;
+import com.example.projetoaos.externaldatabase.demo.service.UserService;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping(value="api/user/")
+@AllArgsConstructor
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("all/")
+    public List<User> getUser(){
+        return userService.getAll();
+    }
+
+    @GetMapping("{id}/")
+    public Optional <User> getUserById(@PathVariable("id") Long id) throws Exception{
+        try{
+            return userService.getUserById(id);
+        } catch(Exception e ) {
+            return null;
+        }
+    }
+
+    @PostMapping("add/")
+    public void insertUser(@RequestBody User user){
+        userService.addUser(user);
+    }
+    
+    @DeleteMapping("user/{id}")
+public void deleteUserById(@PathVariable("id") Long id) throws Exception{
+        try {
+            userService.deleteUserById(id);
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @PutMapping("user/{id}")
+    public boolean updateTitleUserById(@PathVariable Long id, @RequestParam String newName) throws Exception{
+        try{
+           return userService.updateNameById(id, newName);
+        } catch(Exception e){
+            return false;
+        }
+    }
     
 }
--- src/main/java/com/example/projetoaos/externaldatabase/demo/service/UserService.java
+++ src/main/java/com/example/projetoaos/externaldatabase/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.projetoaos.externaldatabase.demo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ public class UserService {
     @Autowired
     public UserRepository userRepository;
 
-    public Optional<User> findUserById(Long id) throws UserNotFoundException{
+    public Optional<User> getUserById(Long id) throws UserNotFoundException{
         Optional <User> opUser = userRepository.findById(id);
         if(opUser.isEmpty()){
             throw new UserNotFoundException("User " + id + " not found");
@@ -31,6 +32,20 @@ public class UserService {
         }
         User u = opUser.get();
         userRepository.delete(u);
+        return true;
+    }
+
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
+    public boolean updateNameById(Long id, String newName){
+        Optional<User> opUser = userRepository.findById(id);
+        if(opUser.isEmpty()){
+            return false;
+        }
+        User u = opUser.get();
+        u.setName(newName);
         return true;
     }
 }
